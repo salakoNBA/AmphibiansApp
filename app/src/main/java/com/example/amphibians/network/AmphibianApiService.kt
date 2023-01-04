80b2a08c470702b7a25b1b8992e20c42567ff16b
oja-daddy <you@example.com>
2023-01-04 15:55:48 +0100

    Working on the ViewModel

--- app/src/main/java/com/example/amphibians/network/AmphibianApiService.kt
+++ app/src/main/java/com/example/amphibians/network/AmphibianApiService.kt
@@ -25,7 +25,7 @@ import retrofit2.http.GET
 
 // TODO: Create a property for the base URL provided in the codelab
 const val BASE_URL = "https://developer.android.com/courses/pathways/android-basics-kotlin-unit-4-pathway-2/"
-const val GET_AMPHIOBIANS = "android-basics-kotlin-unit-4-pathway-2-project-api.json"
+const val GET_AMPHIBIANS = "android-basics-kotlin-unit-4-pathway-2-project-api.json"
 
 // TODO: Build the Moshi object with Kotlin adapter factory that Retrofit will be using to parse JSON
 private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
@@ -37,13 +37,13 @@ private val retrofit: Retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConvert
 
 interface AmphibianApiService {
     // TODO: Declare a suspended function to get the list of amphibians
-    @GET(GET_AMPHIOBIANS)
-    suspend fun getAmphibains(): List<Amphibian>
+    @GET(GET_AMPHIBIANS)
+    suspend fun getAmphibians(): List<Amphibian>
 }
 
 // TODO: Create an object that provides a lazy-initialized retrofit service
 
-object AmphibainApi{
+object AmphibianApi{
     val retrofitService: AmphibianApiService by lazy {
         retrofit.create(AmphibianApiService::class.java)
     }
--- app/src/main/java/com/example/amphibians/ui/AmphibianViewModel.kt
+++ app/src/main/java/com/example/amphibians/ui/AmphibianViewModel.kt
@@ -15,24 +15,56 @@
  */
 package com.example.amphibians.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.amphibians.network.Amphibian
+import com.example.amphibians.network.AmphibianApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 enum class AmphibianApiStatus {LOADING, ERROR, DONE}
 
 class AmphibianViewModel : ViewModel() {
 
     // TODO: Create properties to represent MutableLiveData and LiveData for the API status
+    private val _status = MutableLiveData<AmphibianApiStatus>()
+    val status: LiveData<AmphibianApiStatus>
+        get() = _status
 
     // TODO: Create properties to represent MutableLiveData and LiveData for a list of amphibian objects
+    private val _amphibians = MutableLiveData<List<Amphibian>>()
+    val amphibians: LiveData<List<Amphibian>>
+        get() = _amphibians
 
     // TODO: Create properties to represent MutableLiveData and LiveData for a single amphibian object.
     //  This will be used to display the details of an amphibian when a list item is clicked
+    private val _amphibian = MutableLiveData<Amphibian>()
+    val amphibian: LiveData<Amphibian>
+        get() = _amphibian
+
 
     // TODO: Create a function that gets a list of amphibians from the api service and sets the
     //  status via a Coroutine
+    private fun getAmphibians(){
+        viewModelScope.launch(Dispatchers.IO){
+            try {
+                _status.postValue(AmphibianApiStatus.DONE)
+                _amphibians.postValue(AmphibianApi.retrofitService.getAmphibians())
+            }
+            catch (e: IOException) {
+                _status.postValue(AmphibianApiStatus.ERROR)
+            }
+        }
+    }
 
     fun onAmphibianClicked(amphibian: Amphibian) {
         // TODO: Set the amphibian object
+        _amphibian.value = amphibian
+    }
+    init {
+        getAmphibians()
     }
 }
